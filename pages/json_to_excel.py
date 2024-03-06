3fc1525d99102a98f1165f8b99d11adb8e04b66a
Your Name <you@example.com>
2024-03-06 18:17:13 +0800

    modified: requirements

---
+++ pages/json_to_excel.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import json
+
+def flatten_objects_recursive(data):
+    flattened_objects = []
+
+    # 遍历每个对象
+    for obj in data:
+        # 将当前对象添加到平铺列表中
+        flattened_objects.append(obj)
+
+        # 如果当前对象的 items 属性不为空，则递归平铺 items 中的对象
+        if obj["items"]:
+            flattened_objects.extend(flatten_objects_recursive(obj["items"]))
+
+        # 清空当前对象的 items 属性，以防止重复添加
+        obj["items"] = []
+
+    return flattened_objects
+
+# st.title("json to excel :sunglasses:")
+
+parse_data = ''
+st.session_state.text_contents = ''
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    parse_data = flatten_objects_recursive(json.load(uploaded_file))
+
+yl_button = st.button('转换预览')
+
+st.download_button(
+    '下载excel', st.session_state.text_contents, 'demo' + '_ddl.sql')
+
+if  yl_button:
+    # 解析json对象
+    st.dataframe(parse_data)
