c54b4c3abd429bdbf117dfdcfd4e0bf70daf0497
QinShower <zhangwenming0906@163.com>
2024-09-09 23:49:52 +0800

    add word cloud

---
+++ pages/app.py
@@ -0,0 +1,58 @@
+from PIL import Image
+import numpy as np
+import pandas as pd
+import jieba
+import matplotlib.pyplot as plt
+import streamlit as st
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+
+def cloud(image, parse_data, max_word, max_font, random):
+    stopwords = set(STOPWORDS)
+    stopwords.update([])
+    
+    wc = WordCloud(font_path='/root/py-tools/images/SIMSUN.TTC',background_color="white", colormap="hot", max_words=max_word, mask=image,
+    stopwords=stopwords, max_font_size=max_font, random_state=random)
+
+    # generate word cloud
+    contents = parse_data['content']
+    word_str = ''
+    for content in contents:
+        seg_list = jieba.cut_for_search(content) # 搜索引擎模式
+        word_str += " ".join(seg_list)  
+    wc.generate(word_str)
+
+    # create coloring from image
+    image_colors = ImageColorGenerator(image)
+
+    # show the figure
+    plt.figure(figsize=(100,100))
+    fig, axes = plt.subplots(1,2, gridspec_kw={'width_ratios': [3, 2]})
+    axes[0].imshow(wc, interpolation="bilinear")
+    # recolor wordcloud and show
+    # we could also give color_func=image_colors directly in the constructor
+    axes[1].imshow(image, cmap=plt.cm.gray, interpolation="bilinear")
+
+    for ax in axes:
+        ax.set_axis_off()
+    st.pyplot()
+
+def main():
+    st.write("# Text Summarization with a WordCloud")
+    max_word = st.sidebar.slider("Max words", 200, 3000, 200)
+    max_font = st.sidebar.slider("Max Font Size", 50, 350, 60)
+    random = st.sidebar.slider("Random State", 30, 100, 42 )
+    image = st.file_uploader("Choose a image file", ['png','jpg','svg'])
+    uploaded_file = st.file_uploader("Choose a csv", ['csv'])
+       
+    if image and uploaded_file is not None:
+        if st.button("Plot"):
+            st.write("### Original image")
+            image = np.array(Image.open(image))
+            parse_data = pd.read_csv(uploaded_file)
+            # st.image(image, width=100, use_column_width=True)
+            st.write("### Word cloud")
+            st.write(cloud(image, parse_data, max_word, max_font, random), use_column_width=True)
+
+if __name__=="__main__":
+  main()
